22e1d80fbab30b84b68115c21ecdbd15da30b574
Dinesh Racharla <96189870+DineshRacharla468@users.noreply.github.com>
2023-11-10 10:27:31 -0600

    upadted the vaues in desserts

--- controllers/desserts.js
+++ controllers/desserts.js
@@ -1,14 +1,21 @@
 var desserts = require("../models/desserts");
 // List of all desserts
-exports.desserts_list = async function(req, res) {
-res.send('NOT IMPLEMENTED: desserts list');
+exports.desserts_list = async function (req, res) {
+  res.send('NOT IMPLEMENTED: desserts list');
 };
-// for a specific desserts.
-exports.dessert_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: desserts detail: ' + req.params.id);
-};
+//for a specific Costume.
+exports.dessert_detail = async function (req, res) {
+  console.log("detail" + req.params.id)
+  try {
+    result = await desserts.findById(req.params.id)
+    res.send(result)
+  } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+  }
+}
 // Handle Costume create on POST.
-exports.dessert_create_post = async function(req, res) {
+exports.dessert_create_post = async function (req, res) {
   console.log(req.body)
   let document = new desserts();
   // We are looking for a body, since POST does not have query parameters.
@@ -19,45 +26,61 @@ exports.dessert_create_post = async function(req, res) {
   document.Calories = req.body.Calories;
   document.Price = req.body.Price;
 
-  try{
-  let result = await document.save();
-  res.send(result);
+  try {
+    let result = await document.save();
+    res.send(result);
   }
-  catch(err){
-  res.status(500);
-  res.send(`{"error": ${err}}`);
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
   }
-  };
-// Handle desserts delete form on DELETE.
-exports.dessert_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: desserts delete DELETE ' + req.params.id);
 };
-// Handle desserts update form on PUT.
-exports.dessert_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: desserts update PUT' + req.params.id);
+// Handle desserts delete form on DELETE.
+exports.dessert_delete = function (req, res) {
+  res.send('NOT IMPLEMENTED: desserts delete DELETE ' + req.params.id);
+};
+// Handle Costume update form on PUT.
+exports.dessert_update_put = async function (req, res) {
+  console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+  try {
+    let toUpdate = await desserts.findById(req.params.id)
+    // Do updates of properties
+    if (req.body.Name)
+      toUpdate.Name = req.body.Name;
+    if (req.body.Calories) toUpdate.Calories = req.body.Calories;
+    if (req.body.Price) toUpdate.Price = req.body.Price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+  } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+  }
 };
 
 // List of all dessertss
-exports.dessert_list = async function(req, res) {
-try{
-thedesserts = await desserts.find();
-res.send(thedesserts);
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+exports.dessert_list = async function (req, res) {
+  try {
+    thedesserts = await desserts.find();
+    res.send(thedesserts);
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
 
 // VIEWS
 // Handle a show all view
-exports.dessert_view_all_Page = async function(req, res) {
-try{
-  thedesserts = await desserts.find();
-res.render('desserts', { title: 'desserts Search Results', results: thedesserts });
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+exports.dessert_view_all_Page = async function (req, res) {
+  try {
+    thedesserts = await desserts.find();
+    res.render('desserts', { title: 'desserts Search Results', results: thedesserts });
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
--- routes/desserts.js
+++ routes/desserts.js
@@ -4,3 +4,5 @@ var router = express.Router();
 /* GET deserts */
 router.get('/', dessert_controlers.dessert_view_all_Page );
 module.exports = router;
+// GET request for one costume.
+router.get('/desserts/:id', dessert_controlers.dessert_detail);
