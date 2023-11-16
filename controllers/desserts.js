cbb70c41757391f1a8b43b0f4f7f50051165d8da
Dinesh Racharla <96189870+DineshRacharla468@users.noreply.github.com>
2023-11-15 21:20:11 -0600

    updated ,deletel,detail ,create

--- controllers/desserts.js
+++ controllers/desserts.js
@@ -35,10 +35,7 @@ exports.dessert_create_post = async function (req, res) {
     res.send(`{"error": ${err}}`);
   }
 };
-// Handle desserts delete form on DELETE.
-exports.dessert_delete = function (req, res) {
-  res.send('NOT IMPLEMENTED: desserts delete DELETE ' + req.params.id);
-};
+
 // Handle Costume update form on PUT.
 exports.dessert_update_put = async function (req, res) {
   console.log(`update on id ${req.params.id} with body
@@ -84,3 +81,74 @@ exports.dessert_view_all_Page = async function (req, res) {
     res.send(`{"error": ${err}}`);
   }
 };
+
+// Handle Costume delete on DELETE.
+exports.dessert_delete = async function(req, res) {
+  console.log("delete " + req.params.id)
+  try {
+  result = await desserts.findByIdAndDelete( req.params.id)
+  console.log("Removed " + result)
+  res.send(result)
+  } catch (err) {
+  res.status(500)
+  res.send(`{"error": Error deleting ${err}}`);
+  }
+  };
+  
+  // Handle a show one view with id specified by query
+exports.dessert_view_one_Page = async function(req, res) {
+  console.log("single view for id " + req.query.id)
+  try{
+  result = await desserts.findById( req.query.id)
+  res.render('dessertdetail',
+  { title: 'Dessert Detail', toShow: result });
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+  };
+
+  // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.dessert_create_Page = function(req, res) {
+console.log("create view")
+try{
+res.render('dessertcreate', { title: 'Dessert Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.dessert_update_Page = async function(req, res) {
+  console.log("update view for item "+req.query.id)
+  try{
+  let result = await desserts.findById(req.query.id)
+  res.render('dessertupdate', { title: 'Dessert Update', toShow: result });
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+  };
+
+  // Handle a delete one view with id from query
+exports.dessert_delete_Page = async function(req, res) {
+console.log("Dessert view for id " + req.query.id)
+try{
+result = await desserts.findById(req.query.id)
+res.render('dessertdelete', { title: 'Dessert Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+  
--- routes/desserts.js
+++ routes/desserts.js
@@ -6,3 +6,11 @@ router.get('/', dessert_controlers.dessert_view_all_Page );
 module.exports = router;
 // GET request for one costume.
 router.get('/desserts/:id', dessert_controlers.dessert_detail);
+/* GET detail costume page */
+router.get('/detail', dessert_controlers.dessert_view_one_Page);
+/* GET create costume page */
+router.get('/create', dessert_controlers.dessert_create_Page);
+/* GET create update page */
+router.get('/update', dessert_controlers.dessert_update_Page);
+/* GET delete costume page */
+router.get('/delete', dessert_controlers.dessert_delete_Page);
